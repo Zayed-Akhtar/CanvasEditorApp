18633462c5d1a614fe949554fc6906c7524211cd
Zayed Akhtar <Zayed.Akhtar@odessainc.com>
2025-08-08 20:45:05 +0530

    CSS changes for app responsiveness

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -8,17 +8,21 @@ import { MdDelete } from "react-icons/md";
 import { LuUndo2 } from "react-icons/lu";
 import { LuRedo2 } from "react-icons/lu";
 import { FaDownload } from "react-icons/fa6";
+import { FaLock } from "react-icons/fa";
+import { FaLockOpen } from "react-icons/fa";
 
 function Toolbar({ addRect, addCircle, addText, togglePen, handleShare, handleDelete, isPenActive, handleColorChange, handleUndo, enableUndo, enableRedo, handleRedo, handleExport, handleLockObject, handleUnlockObject}) {
     return (
         <div className="toolbar">
-            <button className="btn btn-outline-primary" onClick={addRect}>Rectangle <PiRectangleDashedLight style={{ fontSize: '1.5rem' }} /></button>
-            <button className="btn btn-outline-secondary" onClick={addCircle}>Circle <TbCircleDashed style={{ fontSize: '1.5rem' }} /></button>
-            <button className="btn btn-outline-dark" onClick={addText}>Text <IoTextOutline style={{ fontSize: '1.5rem' }} /></button>
+            <button className="btn btn-outline-primary" onClick={addRect}>Rectangle <PiRectangleDashedLight  /></button>
+            <button className="btn btn-outline-secondary" onClick={addCircle}>Circle <TbCircleDashed  /></button>
+            <button className="btn btn-outline-dark" onClick={addText}>Text <IoTextOutline  /></button>
             <button className="btn btn-outline-dark" onClick={togglePen}>
                 {isPenActive ? "Disable Pen" : "Pen Tool"}
-                <FaPenAlt style={{ fontSize: '1.5rem' }} />
+                <FaPenAlt  />
             </button>
+            <button className='btn btn-outline-info' title='Lock selected object' onClick={handleLockObject}>Lock <FaLock /></button>
+            <button className='btn btn-outline-info' title='unlock all' onClick={handleUnlockObject}>Unlock <FaLockOpen /></button>
             <div className='colorpicker'>
             <input
                 type="color"
@@ -28,14 +32,11 @@ function Toolbar({ addRect, addCircle, addText, togglePen, handleShare, handleDe
             />
             <span>pick color</span>
             </div>
-            <button disabled={enableUndo < 2} className='btn btn-warning' title='Undo' onClick={handleUndo}><LuUndo2 style={{ fontSize: '1.5rem' }}/></button>
-            <button disabled={!enableRedo} className='btn btn-warning' title='Redo' onClick={handleRedo}><LuRedo2 style={{ fontSize: '1.5rem' }}/></button>
-            <button className='btn btn-danger' title='Delete' onClick={handleDelete}><MdDelete style={{ fontSize: '1.5rem' }} /></button>
+            <button disabled={enableUndo < 2} className='btn btn-warning' title='Undo' onClick={handleUndo}><LuUndo2 /></button>
+            <button disabled={!enableRedo} className='btn btn-warning' title='Redo' onClick={handleRedo}><LuRedo2 /></button>
+            <button className='btn btn-danger' title='Delete' onClick={handleDelete}><MdDelete  /></button>
             <button className="btn btn-success" title='Download as PNG' onClick={handleExport}><FaDownload /></button>
-            <button className="btn btn-primary" title='Share Canvas' onClick={handleShare}>Share <FaRegShareFromSquare style={{ fontSize: '1.5rem' }} /></button>
-            <button onClick={handleLockObject}>Lock</button>
-            <button onClick={handleUnlockObject}>Unlock</button>
-
+            <button className="btn btn-primary" title='Share Canvas' onClick={handleShare}>Share <FaRegShareFromSquare  /></button>
         </div>
     )
 }
